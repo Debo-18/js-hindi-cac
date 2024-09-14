74ad9d8966a6c31b902440faa5d1fb0c0f35163d
Debo-18 <debopratimdam@gmail.com>
2024-09-14 10:45:37 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,23 @@
+// let score = "Debo"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);    // (not a number)
+
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1 ; False => 0
+
+
+// let isloggedin = "Debo"
+
+// let booleanIsLoggedIn = Boolean(isloggedin)
+// console.log(booleanIsLoggedIn)
+
+
+let num = 23
+let bar = String(num)
+console.log(bar);
+console.log(typeof bar);
