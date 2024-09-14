e3ffce6b133269dc73f555293c437b503df77d89
Debo-18 <debopratimdam@gmail.com>
2024-09-14 09:03:17 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs ,not browser
+
+console.log( 3 + 3 );
+console.log("Debo");  // code readability should be high.
+
+let name = "Debo"
+let age = 19
+let isloggedin = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null);   // object
+console.log(typeof undefined);  // undefined
