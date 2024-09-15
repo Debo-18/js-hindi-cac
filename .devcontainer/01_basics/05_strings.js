aba8664380d986827087c95497df95a59bcb42e2
Debo-18 <debopratimdam@gmail.com>
2024-09-15 08:06:41 +0000

    strings

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = "Debo"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Debopratim")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(3));
+
+const newstring = gameName.substring(0,4)
+console.log(newstring);
