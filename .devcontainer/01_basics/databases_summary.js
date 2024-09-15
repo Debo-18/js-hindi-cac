40185e62d6920794ee059e436a92020302d3b9a5
Debo-18 <debopratimdam@gmail.com>
2024-09-15 06:37:20 +0000

    database summary

---
+++ .devcontainer/01_basics/databases_summary.js
@@ -0,0 +1,26 @@
+// Primitive
+
+//  7 types : String, number, boolean, null, undefined, symbol, BigInt
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+
+// *********************************************************
+
+//  Stack (Primitive) , Heap (Non Primitive)
+
+let myyoutubename = "DebopratimDamdotcom"
+
+let anothername = myyoutubename
+anothername = "chaiaurcode"
+
+console.log(anothername);
+console.log(myyoutubename);
+
+// let userone = {
+//     email : "user@google.com"
+//     upi : "user@ybl"
+// }
